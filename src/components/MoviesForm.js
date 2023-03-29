7c25e2d38cb03c17d3d50badb6987d0e9950803a
Marek Witkowski <marek.witkowski@intersynergy.pl>
2023-03-29 08:51:52 +0200

    change Movies Form to hooks

--- src/components/MoviesForm.js
+++ src/components/MoviesForm.js
@@ -1,13 +1,16 @@
 import {createRef} from "react";
 import {moviesActions} from "../redux/movies";
 import {connect} from "react-redux";
+import {useDispatch} from "react-redux";
 
 const MoviesForm = ({addMovie}) => {
     const movieInputRef = createRef();
+    const dispatch = useDispatch();
 
     const addMovieHandler = (event) => {
         event.preventDefault();
-        addMovie(movieInputRef.current.value);
+        // addMovie(movieInputRef.current.value);
+        dispatch(moviesActions.add(movieInputRef.current.value));
         movieInputRef.current.value = "";
     }
 
@@ -19,10 +22,11 @@ const MoviesForm = ({addMovie}) => {
     )
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addMovie: (movie) => dispatch(moviesActions.add(movie))
-    }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         addMovie: (movie) => dispatch(moviesActions.add(movie))
+//     }
+// }
 
-export default connect(null, mapDispatchToProps)(MoviesForm);
+// export default connect(null, mapDispatchToProps)(MoviesForm);
+export default MoviesForm;
